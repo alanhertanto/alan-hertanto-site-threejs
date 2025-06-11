ff282891cb4f22d93a5b6a46f10d98d17dd0ab59
alan-nusameta <alan.hertanto@nusameta.io>
2025-06-11 17:07:52 +0700

    First Commit

---
+++ helper.js
@@ -0,0 +1,28 @@
+// helper.js
+import * as THREE from 'three';
+
+export function setWorldRotation(object, xDeg, yDeg, zDeg) {
+    const worldEuler = new THREE.Euler(
+        THREE.MathUtils.degToRad(xDeg),
+        THREE.MathUtils.degToRad(yDeg),
+        THREE.MathUtils.degToRad(zDeg),
+        'XYZ'
+    );
+
+    const worldQuat = new THREE.Quaternion().setFromEuler(worldEuler);
+
+    const parentQuat = object.parent
+        ? object.parent.getWorldQuaternion(new THREE.Quaternion())
+        : new THREE.Quaternion();
+
+    const localQuat = parentQuat.invert().multiply(worldQuat);
+    object.setRotationFromQuaternion(localQuat);
+}
+
+export function setLocalRotation(object, xDeg, yDeg, zDeg) {
+    object.rotation.set(
+        THREE.MathUtils.degToRad(xDeg),
+        THREE.MathUtils.degToRad(yDeg),
+        THREE.MathUtils.degToRad(zDeg)
+    );
+}
