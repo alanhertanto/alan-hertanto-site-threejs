5adca5347b155c9ebcf6c1715348d592b424bfe1
alanhertanto <alanhertantog@gmail.com>
2025-06-15 18:52:49 +0700

    refactor(core): split Main.js and helper.js to modularize scripts for avoiding god classes

---
+++ rendererController.js
@@ -0,0 +1,12 @@
+import * as THREE from 'three';
+let renderer;
+export function initRendererControls() {
+    renderer = new THREE.WebGLRenderer({ antialias: false });
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.shadowMap.enabled = true;
+    renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+    document.body.appendChild(renderer.domElement);
+}
+export function getRendererControls() {
+    return renderer;
+}
