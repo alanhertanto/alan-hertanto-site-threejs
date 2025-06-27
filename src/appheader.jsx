f636896e739d7a54c1788b2b09d9d7940f6fd43f
alanhertanto <alanhertantog@gmail.com>
2025-06-28 02:31:23 +0700

    feat(UI) : remove shadcdn because incompability, adding daisyUI for better compability

--- src/appheader.jsx
+++ src/appheader.jsx
@@ -1,29 +1,2 @@
-import {
-  Menubar,
-  MenubarContent,
-  MenubarItem,
-  MenubarMenu,
-  MenubarSeparator,
-  MenubarShortcut,
-  MenubarTrigger,
-} from "@/components/ui/menubar"
-
 export default function Appheader(){
-    return (
-    <Menubar>
-        <MenubarMenu>
-            <MenubarTrigger>File</MenubarTrigger>
-            <MenubarContent>
-            <MenubarItem>
-                New Tab <MenubarShortcut>⌘T</MenubarShortcut>
-            </MenubarItem>
-            <MenubarItem>New Window</MenubarItem>
-            <MenubarSeparator />
-            <MenubarItem>Share</MenubarItem>
-            <MenubarSeparator />
-            <MenubarItem>Print</MenubarItem>
-            </MenubarContent>
-        </MenubarMenu>
-    </Menubar>
-    );
 }
